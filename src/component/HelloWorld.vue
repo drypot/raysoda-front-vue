acaa7d63f73bb94c6a46381d87a7b7310b0ee888
drypot <drypot@gmail.com>
2021-09-27 17:39:10 +0900

    update environment setting

--- src/component/HelloWorld.vue
+++ src/component/HelloWorld.vue
@@ -1,11 +1,3 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
-
 <template>
   <h1 class="text-4xl">{{ msg }}</h1>
   <div class="mt-4">
@@ -13,5 +5,10 @@ const count = ref(0)
   </div>
 </template>
 
-<style scoped>
-</style>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': 'http://localhost:2400',
+    },
+  },
   plugins: [vue()]
 })
